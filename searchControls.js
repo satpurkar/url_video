7a809452722d742067c6682efc3e1a38a2346bfa
satpurkar <satpurkaraditya@gmail.com>
2018-10-11 19:34:23 +0530

    add new variable default_url

--- searchControls.js
+++ searchControls.js
@@ -1,11 +1,12 @@
 window.onload = function(){
-			var source = "https://media.w3.org/2010/05/sintel/trailer.mp4"; //default video
+			var default_url = "https://media.w3.org/2010/05/sintel/trailer.mp4"; 
+			var source = default_url; //default video
 			document.getElementById('videoSource').setAttribute("src", source);
 			document.getElementById('video').load();
 		}
 		
 function reload(){
-
+ 	// document.getElementById('searchInput').setAttribute("placeholder", default_url);
 	var url = document.getElementById('searchInput').value;
 	document.getElementById('videoSource').setAttribute("src", url);
  	document.getElementById('video').load();
